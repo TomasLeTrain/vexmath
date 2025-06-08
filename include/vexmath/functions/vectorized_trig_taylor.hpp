bf0a22cf5aea1416b38f26cd415f84de10f15223
TomasLeTrain <tomylopezd@gmail.com>
2025-06-07 22:59:11 -0400

    optimize vectorized taylor trig

--- include/vexmath/functions/vectorized_trig_taylor.hpp
+++ include/vexmath/functions/vectorized_trig_taylor.hpp
@@ -6,6 +6,40 @@
 #pragma once
 #include <arm_neon.h>
 
+/* possible formulations
+ * total instructions: 6
+ 
+    *ysin = (xsin + t * xcos) - xsin * (t * t * Vhalf);
+    *ycos = (xcos - t * xsin) - xcos * (t * t * Vhalf);
+
+    ----------
+    
+    1 -> t2 = vmul t, t
+    2 -> t2_half = vmul t2, vhalf
+    
+           4     3                               
+    sin = vmls (vmla xsin, t, xcos), xsin, t2_half
+           6     5                               
+    cos = vmls (vmls xcos, t, xsin), xcos, t2_half
+*/
+
+/* different formulation - implemented
+ * total instructions: 5
+
+    *ysin = xsin + t * (xcos - xsin * t * Vhalf);
+    *ycos = xcos - t * (xsin - xcos * t * Vhalf);
+    
+    ----------
+
+    1 -> t_half = vmul t, half
+    
+            3              2
+    sin = vmla xsin, t, (vmls xcos, xsin, t_half)
+            5              4
+    cos = vmls xcos, t, (vmls xsin, xcos, t_half)
+*/
+
+
 /**
  * @brief approximates sin and cos for x
  *
@@ -23,14 +57,31 @@ inline void Vsincos_taylor(float32x4_t x,
                            float32x4_t xcos,
                            float32x4_t* ysin,
                            float32x4_t* ycos) {
-    // sin = sin(c) + cos(c) (x-c) - sin(c) (x-c)^2/2
-    // cos = cos(c) - sin(c) (x-c) - cos(c) (x-c)^2/2
-    //
-    static const float32x4_t Vhalf = vmovq_n_f32(0.5);
 
-    float32x4_t t = (x - c);
-    *ysin = xsin + xcos * t - xsin * t * t * Vhalf;
-    *ycos = xcos - xsin * t - xcos * t * t * Vhalf;
+    float32x4_t t = vsubq_f32(x, c);
+    // t * 0.5
+    float32x4_t t_half = vmulq_n_f32(t, 0.5f);
+
+    float32x4_t sin = xcos;
+    float32x4_t cos = xsin;
+
+    // sin = xcos - xsin * (t * 0.5)
+    // cos = xsin - xcos * (t * 0.5)
+    sin = vmlsq_f32(sin, xsin, t_half);
+    cos = vmlsq_f32(cos, xcos, t_half);
+
+    // theoretically slower due to pointer dereference + more mov instructions
+    // in practice this should be inlined and optimized by the compiler avoiding mul and add instructions
+    *ysin = xsin;
+    *ycos = xcos;
+
+    *ysin = vmlaq_f32(*ysin,t,sin);
+    *ycos = vmlsq_f32(*ycos,t,cos);
+
+    // sin = vmulq_f32(t,sin);
+    // cos = vmulq_f32(t,cos);
+    // *ysin = vaddq_f32(xsin,sin);
+    // *ycos = vsubq_f32(xcos,cos);
 }
 
 /**
@@ -48,10 +99,28 @@ inline void Vsincos_taylor_delta(float32x4_t t,
                                  float32x4_t xcos,
                                  float32x4_t* ysin,
                                  float32x4_t* ycos) {
-    // sin = sin(c) + cos(c) (x-c) - sin(c) (x-c)^2/2
-    // cos = cos(c) - sin(c) (x-c) - cos(c) (x-c)^2/2
-    static const float32x4_t Vhalf = vmovq_n_f32(0.5);
 
-    *ysin = xsin + xcos * t - xsin * t * t * Vhalf;
-    *ycos = xcos - xsin * t - xcos * t * t * Vhalf;
+    // t * 0.5
+    float32x4_t t_half = vmulq_n_f32(t, 0.5f);
+
+    float32x4_t sin = xcos;
+    float32x4_t cos = xsin;
+
+    // sin = xcos - xsin * (t * 0.5)
+    // cos = xsin - xcos * (t * 0.5)
+    sin = vmlsq_f32(sin, xsin, t_half);
+    cos = vmlsq_f32(cos, xcos, t_half);
+
+    // theoretically slower due to pointer dereference + more mov instructions
+    // in practice this should be inlined and optimized by the compiler avoiding mul+add instructions
+    *ysin = xsin;
+    *ycos = xcos;
+
+    *ysin = vmlaq_f32(*ysin,t,sin);
+    *ycos = vmlsq_f32(*ycos,t,cos);
+
+    // sin = vmulq_f32(t,sin);
+    // cos = vmulq_f32(t,cos);
+    // *ysin = vaddq_f32(xsin,sin);
+    // *ycos = vsubq_f32(xcos,cos);
 }
