b5f2acdb11c94094fa92508a1638728f511ebb08
TomasLeTrain <tomylopezd@gmail.com>
2025-10-10 18:23:13 -0400

    made dummy_var actually work

---
+++ src/vexmath/dummy.cpp
@@ -0,0 +1,6 @@
+// read https://github.com/LemLib/units/blob/main/src/units/dummy.cpp for more
+// info
+
+namespace {
+volatile char vexmath_dummy_var;
+}
--- src/vexmath/dummy.hpp
+++
@@ -1,5 +0,0 @@
-// read https://github.com/LemLib/units/blob/main/src/units/dummy.cpp for more info
-
-namespace {
-volatile char dummy_var;
-}
